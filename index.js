74e3f3b30378a6486260b4b576b4e7a5cc081a3a
doaamelhem96 <drduaamelhem@gamil.com>
2023-04-14 13:05:29 +0000

    Delete

--- index.js
+++ index.js
@@ -15,11 +15,22 @@ const PORT = process.env.PORT;
 // const client = new Client(url);
 app.get('/', test);
 
+app.delete('/deleteNews/:id',deleteNewsHandler)
 function test(req, res){
     res.send('Hello team');
 }
 
 
+function deleteNewsHandler(req,res)
+{
+    let {id}= req.params;
+    let sql =`DELETE FROM News WHERE id =$1;`;
+    let value =[id];
+    client.query(sql,value) .then(result =>
+        {res.status(204).send("successfully Deleted")})
+         .catch();
+
+}
 app.listen(PORT, () => {
     console.log("Welcome to my server");
 }) 
