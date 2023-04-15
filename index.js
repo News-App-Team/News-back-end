08c1d18527615d0d3890d573bad9d3cd913468a2
hasanalrawaqa <hasanalrawaqa@gmail.com>
2023-04-15 22:15:48 +0000

    add getdbHandelr

--- index.js
+++ index.js
@@ -4,22 +4,37 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const bodyParser = require('body-parser');
-const {Client} = require('pg');
+const { Client } = require('pg');
 require('dotenv').config();
-
 const app = express();
 app.use(cors());
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const PORT = process.env.PORT;
-// const client = new Client(url);
-app.get('/', test);
+let url = process.env.dburl;
+const client = new Client(url);
+app.get ('/getDbNews', getDbNewsHandler)
 
-function test(req, res){
-    res.send('Hello team');
+
+
+
+
+function getDbNewsHandler(req, res) {
+    let sql = `SELECT * FROM news;`;
+    client
+        .query(sql)
+        .then((result) => {
+            res.json(result.rows);
+            console.log(result.rows);
+        })
+        .catch(() => {
+            res.status(500).send("Internal server error");
+        });
 }
 
-
-app.listen(PORT, () => {
-    console.log("Welcome to my server");
-}) 
+client.connect()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log("Welcome to my server");
+    })
+})
