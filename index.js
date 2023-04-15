21260e49e0d0e92f6dd67030023fa0570acc8389
leenalzaben <leena.zaben61@gmail.com>
2023-04-15 17:14:33 +0000

    add_post_route

--- index.js
+++ index.js
@@ -4,22 +4,35 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const bodyParser = require('body-parser');
-const {Client} = require('pg');
 require('dotenv').config();
-
+const { Client } = require('pg');
+const client = new Client(process.env.DATABASE_URL);
 const app = express();
 app.use(cors());
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const PORT = process.env.PORT;
-// const client = new Client(url);
-app.get('/', test);
 
-function test(req, res){
-    res.send('Hello team');
+app.post('/addNews', addNewsHandler);
+
+function addNewsHandler(req, res) {
+    let { source, author, title, description, url, image, publishedat, comment } = req.body;
+    let sql = `INSERT INTO news(source,author, title, description, url,image,publishedat,comment) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *`;
+    let values = [source, author, title, description, url, image, publishedat, comment];
+    client.query(sql, values)
+        .then((result) => {
+            res.status(201).json(result.rows);
+
+        })
+        .catch(() => {
+
+        });
+
 }
+client.connect().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Welcome to my server ${PORT}`);
+    })
 
+})
 
-app.listen(PORT, () => {
-    console.log("Welcome to my server");
-}) 
